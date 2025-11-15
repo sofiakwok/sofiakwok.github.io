91c9128854d1c35a05efc30928eac94e13202512
sofiakwok <zazakwok@gmail.com>
2025-11-15 14:06:02 -0800

    projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,70 +1,59 @@
 import React from "react";
-import mock01 from '../assets/images/mock01.png';
-import mock02 from '../assets/images/mock02.png';
-import mock03 from '../assets/images/mock03.png';
-import mock04 from '../assets/images/mock04.png';
-import mock05 from '../assets/images/mock05.png';
-import mock06 from '../assets/images/mock06.png';
-import mock07 from '../assets/images/mock07.png';
-import mock08 from '../assets/images/mock08.png';
-import mock09 from '../assets/images/mock09.png';
-import mock10 from '../assets/images/mock10.png';
+
+import bananabot from '../assets/images/projects/bananabot.jpg'
+import elementary from '../assets/images/projects/elementary.png'
+import honeybee from '../assets/images/projects/honeybee.jpg'
+import sumobot from '../assets/images/projects/sumobot_2.jpg'
+import puzzlebot from '../assets/images/projects/puzzlebot.jpg'
+import kidnapped from '../assets/images/projects/kidnapped.jpg'
+import biped from '../assets/images/projects/bolt_rw.jpg'
+
 import '../assets/styles/Project.scss';
 
 function Project() {
     return(
     <div className="projects-container" id="projects">
-        <h1>Personal Projects</h1>
+        <h1>Projects</h1>
         <div className="projects-grid">
             <div className="project">
-                <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><img src={mock10} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><h2>Filmate AI</h2></a>
-                <p>Developed movie finder app with semantic search and sentiment analysis using OpenAI GPT-3.5 Turbo, Qdrant, React, and Flask.</p>
+                <a href="https://rexlab.ri.cmu.edu/" target="_blank" rel="noreferrer"><img src={biped} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://rexlab.ri.cmu.edu/" target="_blank" rel="noreferrer"><h2>Biped</h2></a>
+                <p>Built and programmed a controller for </p>
             </div>
             <div className="project">
-                <a href="https://yujisatojr.itch.io/highspeedchase" target="_blank" rel="noreferrer"><img src={mock09} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://yujisatojr.itch.io/highspeedchase" target="_blank" rel="noreferrer"><h2>High Speed Chase</h2></a>
-                <p>Designed, developed, and launched a 3D multiplayer racing game with C# and Unity. This is available on Itch.io for gamers worldwide to enjoy.</p>
+                <a href="https://ascelibrary.org/doi/10.1061/9780784484470.039" target="_blank" rel="noreferrer"><img src={honeybee} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://ascelibrary.org/doi/10.1061/9780784484470.039" target="_blank" rel="noreferrer"><h2>Honeybee Robotics</h2></a>
+                <p>Supported redesign, validation, and testing of RESOURCE, an in-situ sampling system for lunar regolith. This drill is able to do in-situ sampling by integrating all of the instruments into the drill bit. Resulted in an increase to TRL 4 from TRL 1 with testing at NASA Ames.</p>
             </div>
             <div className="project">
-                <a href="https://yujisatojr.itch.io/spacecraft" target="_blank" rel="noreferrer"><img src={mock08} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://yujisatojr.itch.io/spacecraft" target="_blank" rel="noreferrer"><h2>Astro Raiders</h2></a>
-                <p>Developed and released a 2D shooting game with C# and Unity. This project is hosted on the Itch.io public marketplace.</p>
+                <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><img src={bananabot} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><h2>Bananabot</h2></a>
+                <p>Made a 6DOF arm play Bananagrams with a custom end effector.</p>
             </div>
             <div className="project">
-                <a href="https://www.datumlearn.com/" target="_blank" rel="noreferrer"><img src={mock07} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://www.datumlearn.com/" target="_blank" rel="noreferrer"><h2>Datum: Integrated Learning Platform</h2></a>
-                <p>This is an online educational platform that provides high-quality, data science-focused learning resources in the Japanese language. I created the entire platform from scratch using Ruby on Rails.</p>
+                <a href="https://me72.caltech.edu/me72/2022" target="_blank" rel="noreferrer"><img src={sumobot} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://me72.caltech.edu/me72/2022" target="_blank" rel="noreferrer"><h2>Sumobot</h2></a>
+                <p>Designed, built, and programmed a "sumo robot" for the capstone design course. Our final design had over 220 pounds of downforce from neodynium magnets and was completely machined by myself and one other team member. Team won final competition undefeated.</p>
             </div>
             <div className="project">
-                <a href="http://www.wemanage.jp/" target="_blank" rel="noreferrer"><img src={mock06} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="http://www.wemanage.jp/" target="_blank" rel="noreferrer"><h2>WeManage: Real Estate Asset Management</h2></a>
-                <p>This mobile application allows realtors in Japan to securely manage their property information and view future income predictions. This app is built with Ruby on Rails and JavaScript.</p>
+                <a href="https://github.com/ME-134" target="_blank" rel="noreferrer"><img src={puzzlebot} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/ME-134" target="_blank" rel="noreferrer"><h2>Puzzlebot</h2></a>
+                <p>Built and programmed a 5DOF robot arm to solve a jigsaw puzzle. Featured a custom vacuum pump and suction cup end-effector that allowed for easy pickup of puzzle pieces.</p>
             </div>
             <div className="project">
-                <a href="https://www.byuh.edu/covid-19-case-management" target="_blank" rel="noreferrer"><img src={mock05} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://www.byuh.edu/covid-19-case-management" target="_blank" rel="noreferrer"><h2>COVID-19 Case Management</h2></a>
-                <p>Built official charts for COVID/vaccination tracking for an educational institution using JavaScript and the Google Sheets API v4. The dashboard served the university's leadership in their decision-making processes.</p>
+                <a href="https://www.datumlearn.com/" target="_blank" rel="noreferrer"><img src={kidnapped} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://www.datumlearn.com/" target="_blank" rel="noreferrer"><h2>Kidnapped Robot Problem</h2></a>
+                <p>Caltech ME/CS/EE 169 Kidnapped robot</p>
             </div>
             <div className="project">
-                <a href="https://github.com/yujisatojr/multi-reg-analysis" target="_blank" rel="noreferrer"><img src={mock04} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://github.com/yujisatojr/multi-reg-analysis" target="_blank" rel="noreferrer"><h2>Multiple Regression Property Analysis</h2></a>
-                <p>Analyzed the real estate market in Japan and predicted property prices by implementing statistical methods such as OLS and multi-regression analysis. This project leveraged Python and various libraries such as Pandas, NumPy, Matplotlib, and Scikit-Learn.</p>
+                <a href="https://bioinstrumentation.mit.edu/" target="_blank" rel="noreferrer"><img src={honeybee} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://bioinstrumentation.mit.edu/" target="_blank" rel="noreferrer"><h2>MIT Bioinstrumentation Lab</h2></a>
+                <p>Designed thermal haptic system using Peltier devices. Wrote a PD controller for temperature control.</p>
             </div>
             <div className="project">
-                <a href="https://holokai.byuh.edu/programs-of-study" target="_blank" rel="noreferrer"><img src={mock03} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://holokai.byuh.edu/programs-of-study" target="_blank" rel="noreferrer"><h2>Programs of Study</h2></a>
-                <p>Designed and developed a custom component for a CMS-based platform (e.g., 'Brightspot') using Java, Handlebars, and LESS. University students can find their majors of interest through this module.</p>
-            </div>
-            <div className="project">
-                <a href="https://hookele.byuh.edu/transfer-evaluation-guidelines-and-matrix" target="_blank" rel="noreferrer"><img src={mock02} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://hookele.byuh.edu/transfer-evaluation-guidelines-and-matrix" target="_blank" rel="noreferrer"><h2>Transfer Evaluation Matrix</h2></a>
-                <p>Created an interactive CSV table generator with Java, Handlebars, and LESS. This project helps transfer students to quickly identify eligible credits.</p>
-            </div>
-            <div className="project">
-                <a href="https://github.com/yujisatojr/submeowrine" target="_blank" rel="noreferrer"><img src={mock01} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://github.com/yujisatojr/submeowrine" target="_blank" rel="noreferrer"><h2>Submeowrine</h2></a>
-                <p>Developed and released an Android mobile application using Java and Android Studio that runs a 2D shooting game.</p>
+                <a href="https://www.elementaryml.com/" target="_blank" rel="noreferrer"><img src={elementary} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://www.elementaryml.com/" target="_blank" rel="noreferrer"><h2>Elementary Robotics</h2></a>
+                <p>Designed, prototyped, and tested a motor dynamometer in order to get accurate estimations on how a standard brushless motor would perform using Elementary’s custom firmware.</p>
             </div>
         </div>
     </div>
